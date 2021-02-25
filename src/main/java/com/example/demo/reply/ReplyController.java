88292bc17776e1078c0898ba26c63899cd460329
Ezen_AY <Ezen_AY@Ezen_AY-PC>
2021-02-25 11:56:45 +0900

    ha

--- src/main/java/com/example/demo/reply/ReplyController.java
+++ src/main/java/com/example/demo/reply/ReplyController.java
@@ -52,7 +52,7 @@ public class ReplyController {
 		}
 
 		return insertCount ==1 
-		? new ResponseEntity<>("success", HttpStatus.OK)
+		? new ResponseEntity<>("success", HttpStatus.OK) 
 		: new ResponseEntity<> (HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
