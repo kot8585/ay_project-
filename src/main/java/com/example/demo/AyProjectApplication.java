f8d3a74651c01be2e631ba7931a34d185b7f39ae
kot8585 <ansgywjd321@gmail.com>
2021-04-01 10:59:53 +0900

    remove testcode
    테스트 코드때문에 war파일이 안만들어져서 삭제

--- src/main/java/com/example/demo/AyProjectApplication.java
+++ src/main/java/com/example/demo/AyProjectApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AyProjectApplication {
+public class AyProjectApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { //war배포를 위한 소스
+		return builder.sources(AyProjectApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AyProjectApplication.class, args);
 	}
